43ab54c33f0f6b8c9c2da25d10d9fb9acef32715
handagenalini <handagenalini@gmail.com>
2023-03-08 19:02:29 +0530

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const http = require('http');
 const express=require('express')
 const app=express()
+const bodyparser=require('body-parser')
+app.use(bodyparser.urlencoded({extended:false}))
+// app.use('/',(req,res,next)=>{
+//     // res.send('<h1>hello</h1>')
+//     next()
+// })
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
+
+    
+    
+})
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>hello from express</h1>')
+
+})
 const server = http.createServer(app)
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    res.send(`<h1>hello to node</h1>`)
-    next()
-})
-app.use((req,res,next)=>{
-    console.log('in the and middleware')
-    
-    
-})
+
 server.listen(3000);
 
