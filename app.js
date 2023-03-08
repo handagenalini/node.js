9b53178360693f40331fd8a937d2b8e782122bee
handagenalini <handagenalini@gmail.com>
2023-03-08 20:54:49 +0530

    first commit

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ app.use(bodyparser.urlencoded({extended:false}))
 
  app.use(adminroutes)
  app.use(shops)
+ app.use((res,req,next)=>{
+    res.status(404).send(`<h1>page not found</h1>`)
+})
 
 const server = http.createServer(app)
+
 
 server.listen(3000);
 
