77bbabf8931725ca688d0b5bdb72cdfb27874c96
handagenalini <handagenalini@gmail.com>
2023-03-08 20:43:16 +0530

    first commit

--- app.js
+++ app.js
@@ -2,26 +2,17 @@ const http = require('http');
 const express=require('express')
 const app=express()
 const bodyparser=require('body-parser')
+const adminroutes=require('./routes/admin.js')
+const shops=require('./routes/shop.js')
 app.use(bodyparser.urlencoded({extended:false}))
 // app.use('/',(req,res,next)=>{
 //     // res.send('<h1>hello</h1>')
 //     next()
 // })
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>')
 
-    
-    
-})
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
+ app.use(adminroutes)
+ app.use(shops)
 
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>hello from express</h1>')
-
-})
 const server = http.createServer(app)
 
 server.listen(3000);
