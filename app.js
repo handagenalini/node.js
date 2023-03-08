ba3ff14755acf0f79717df3b485e244e42c9a48b
handagenalini <handagenalini@gmail.com>
2023-03-08 14:55:48 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const http = require('http');
-const routes=require('./routes')
-
-const server = http.createServer(routes)
+const express=require('express')
+const app=express()
+const server = http.createServer(app)
+app.use((req,res,next)=>{
+    console.log('in the middleware')
+    res.send(`<h1>hello to node</h1>`)
+    next()
+})
+app.use((req,res,next)=>{
+    console.log('in the and middleware')
+    
+    
+})
 server.listen(3000);
 
